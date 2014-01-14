c271b8007f76294c46957e251258ce187872de54
RyanPMcD <ryan.patrick.mcdonald@gmail.com>
2014-01-14 14:34:30 -0500

    expanded on the files being compared

--- phish_shows.rb
+++ phish_shows.rb
@@ -1,18 +1,26 @@
 require 'csv'
 
-file1 = "./danis_shows.csv"
-file2 = "./ryans_shows.csv"
-
+file1 = "./daves_shows.csv"
+file2 = "./danis_shows.csv"
+file3 = "./ryans_shows.csv"
+first = []
 show = []
 CSV.foreach(file1) do |row1|
 	CSV.foreach(file2) do |row2|
 		if row1 == row2
-			show << "#{row2[0]} #{row2[1]}"
+			first << row2
 		end
 	end
 end
-File.open('results.rtf', 'w') do |f|
-	f.puts "We attended: "
+CSV.foreach(file3) do |row3|
+	first.each do |x|
+		if x == row3
+			show << "#{row3[0]} #{row3[1]}"
+		end
+	end
+end
+File.open('results4.rtf', 'w') do |f|
+	f.puts "We all attended:"
 	show.each do |s|    
 		f.puts "#{s} together"
 	end
